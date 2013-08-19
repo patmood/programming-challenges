640262fb08ee8f3811a871d04b0c76672f6719ff
patmood <patrick.n.moody@gmail.com>
2013-08-19 15:14:08 -0700

    Complete working solution with tests

--- fibonacci/fib.rb
+++ fibonacci/fib.rb
@@ -5,8 +5,9 @@ def fib(n)
     0
   elsif n == 1
     1
+  else
+    fib(n-1) + fib(n-2)
   end
-
 end
 
 #tests
@@ -16,5 +17,9 @@ end
 
 assert(fib(0) == 0)
 assert(fib(1) == 1)
+assert(fib(2) == 1)
+assert(fib(3) == 2)
+assert(fib(4) == 3)
+assert(fib(9) == 34)
 p "All ok"
 
