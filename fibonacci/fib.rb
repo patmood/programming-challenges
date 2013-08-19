b0ceb9ab665730fb64718be3b57b02c1642e720e
patmood <patrick.n.moody@gmail.com>
2013-08-19 15:03:31 -0700

    Base cases

---
+++ fibonacci/fib.rb
@@ -0,0 +1,15 @@
+# This program will calculate the n-th number in the fibonacci sequence
+
+def fib(n)
+  if n == 0
+    0
+  elsif n == 1
+    1
+  end
+
+end
+
+#tests
+p fib(0) == 0
+p fib(1) == 1
+
