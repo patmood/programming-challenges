3a42f930fe62c932fa2f6bf7fea879c988b721b4
patmood <patrick.n.moody@gmail.com>
2013-09-09 13:34:16 -0700

    cleaner reduce example

--- js_reduce/reduce.js
+++ js_reduce/reduce.js
@@ -1,8 +1,7 @@
 var array = [1,2,3,4,5,6]
 
 var sum_array = array.reduce(function(previousValue,currentValue){
-  previousValue += currentValue
-  return previousValue
+  return previousValue + currentValue
 })
 
 console.log("Sum of array:",sum_array)
